1bf70a1626e638b6f710158f289ff83426a1998a
Ryan Possardt <rpossardt@gmail.com>
2016-02-01 14:26:20 -0500

    Made java project

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame j = new JFrame("Pong");
+		j.setSize(1800, 900);
+		j.setVisible(true);
+	}
+
+}
