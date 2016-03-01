f406c3b3e49e5c657dfc6b747280a31b097abb75
Scott Cote <scott.cote@uconn.edu>
2016-03-01 13:23:58 -0500

    Worked on collision detection after investigating grid

--- src/ryanPossardt/CollisionDetection.java
+++ src/ryanPossardt/CollisionDetection.java
@@ -4,18 +4,27 @@ public class CollisionDetection {
 	public void hitPaddle(Ball ball, Paddle paddle){
 		//if ball hits paddle
 		if(paddle.getPaddleX() < 100){
-			if((paddle.getPaddleX() - ball.getBallx() > -40) && (Math.abs(paddle.getPaddleY() - ball.getBally() )< 140)){
+			if((paddle.getPaddleX() - ball.getBallx() > -40) && yHit(ball, paddle)){
 				System.out.println("collision detected");
 				ball.setDirectionballx(ball.getDirectionballx() + 1);
 				ball.setBallSpeed(ball.getBallSpeed() + 1);
 			}
 		}else{
-			if((paddle.getPaddleX() - ball.getBallx() < 50) && (Math.abs(paddle.getPaddleY() - ball.getBally() )< 140)){
+			if((paddle.getPaddleX() - ball.getBallx() < 50) && yHit(ball, paddle)){
 				System.out.println("collision detected");
 				ball.setDirectionballx(ball.getDirectionballx() + 1);
 				ball.setBallSpeed(ball.getBallSpeed() + 1);
 			}
 		}
-
 	}
+	
+	private boolean yHit(Ball ball, Paddle paddle){
+		boolean hit = true;
+		int delta = ball.getBally()-paddle.getPaddleY() + 25;
+		if (delta < 0 || delta > 140){
+			hit = false;
+		}
+		return hit;
+	}
+	
 }
