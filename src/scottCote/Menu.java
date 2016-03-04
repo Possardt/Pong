11705fe0cf2c06c3cf9e6af1321286143602757d
Scott Cote <scott.cote@uconn.edu>
2016-03-04 10:21:38 -0500

    aligned menu buttons

--- src/scottCote/Menu.java
+++ src/scottCote/Menu.java
@@ -1,6 +1,8 @@
 package scottCote;
 
 import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -13,19 +15,31 @@ public class Menu extends JPanel implements ActionListener {
 	private JButton startButton;
 	private JButton exitButton;
 
+	
+	private GridBagConstraints getGBC(int x, int y){
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = x;
+		gbc.gridy = y;
+		return gbc;
+	}
+
 	public Menu(){
 		setGameRunning(false);
 		setBackground(Color.BLACK);
 		setFocusable(true);
+		this.setLayout(new GridBagLayout());
+		
+
 		
 		this.startButton = new JButton("New Game");
 		this.startButton.addActionListener(this);
-		this.add(startButton);
+		this.add(this.startButton, getGBC(0,1));
 		
 		this.exitButton = new JButton("Exit");
 		this.exitButton.addActionListener(this);
-		this.add(exitButton);
+		this.add(this.exitButton, getGBC(0,2));
 	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e){
 		Object source = e.getSource();
